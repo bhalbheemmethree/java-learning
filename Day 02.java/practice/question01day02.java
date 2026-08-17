a91e47541b487c953136ee06f76ffb9895551277
bhalbheemmethree-star <bhalbheemmethree@gmail.com>
2026-08-18 01:53:52 +0530

    Complete Java Day 02 - Program Structure and Basics

---
+++ Day 02.java/practice/question01day02.java
@@ -0,0 +1,34 @@
+/*
+QUESTION 01 — STUDENT PROFILE
+
+Write a Java program that creates a class named Student
+and prints a student's profile.
+
+Expected output:
+
+Student Profile
+Name: Bhalbheem
+Course: CSE
+Language: Java
+
+Requirements:
+- Use a properly named class.
+- Use main() correctly.
+- Use meaningful identifiers.
+- Use at least one comment.
+*/
+public class question01day02{
+
+    public static void main(String[] args) {
+
+        // Student profile
+        String studentName = "Bhalbheem";
+        String courseName = "CSE";
+        String programmingLanguage = "Java";
+
+        System.out.println("Student Profile");
+        System.out.println("Name: " + studentName);
+        System.out.println("Course: " + courseName);
+        System.out.println("Language: " + programmingLanguage);
+    }
+}
