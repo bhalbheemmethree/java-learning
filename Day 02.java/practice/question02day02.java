ca50805bfde9840a912ecf3a7e54572c28c82055
bhalbheemmethree-star <bhalbheemmethree@gmail.com>
2026-08-18 01:59:24 +0530

    Complete Java Day 02 - Program Structure and Basics

--- Day 02.java/practice/question02day02.java
+++ Day 02.java/practice/question02day02.java
@@ -1,3 +1,32 @@
-public class question02 {
-    
+// QUESTION 02 — FIX THE PROGRAM
+
+// The following Java program contains multiple errors.
+
+// Find and correct all the errors so that the program
+// compiles and runs successfully.
+
+// Identify what rule was violated by each error.
+
+
+// public class question02day02 {
+
+    // public static void main(String[] args) {
+
+        // int student age = 17;
+
+        // System.out.println("Age: " + student age);
+
+    // }
+// }
+
+
+public class question02day02 {
+
+    public static void main(String[] args) {
+
+        int studentAge = 17;
+
+        System.out.println("Age: " + studentAge);
+
+    }
 }
