4cdb11d4fe99cd0428bda7399ec750b4fb7b9e1e
bhalbheemmethree-star <bhalbheemmethree@gmail.com>
2026-08-20 23:09:18 +0530

    Complete java Day 03 - Variables and Data types

---
+++ Day 03.java/practice/question01day03.java
@@ -0,0 +1,18 @@
+public class question01day03 {
+    
+    public static void main(String[] args) {
+
+       String Name = "Bhalbheem";
+       int Age = 20;
+       double  CGPA = 9.0;
+       String Grade = "A+";
+       boolean isPassed = true;
+
+        System.out.println("Name: " + Name);
+        System.out.println("Age: " + Age);
+        System.out.println("CGPA: " + CGPA);
+        System.out.println("Grade: " + Grade);
+        System.out.println("Is Passed: " + isPassed);
+    }
+    
+}
---
+++ Day 03.java/practice/question02day03.java
@@ -0,0 +1,30 @@
+public class question02day03 {
+
+    public static void main(String[] args) {
+
+        int age;
+        age = 21;
+
+        String name = "Bhalbheem";
+
+        double salary;
+        salary = 50000.50;
+        salary = 60000.75;
+
+        double experience = 5.5;
+
+        char grade = 'A';
+        boolean isworking = true;
+
+        System.out.println("Employee Details");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Experience: " + experience);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Working: " + isworking);
+
+    
+    }
+
+}
